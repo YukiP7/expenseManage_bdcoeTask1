a4bd6c3c339811b1157b563ab74181fe5e6a1e91
YukiP7 <gargyukti112@gmail.com>
2023-10-15 16:15:57 +0530

    Add media

--- script.js
+++ script.js
@@ -13,13 +13,33 @@ form.addEventListener('submit' , function(e){
         const item = document.createElement('li') 
         item.innerHTML = `<span>${descrip} ${amount}</span>`
         document.querySelector('#ans').appendChild(item) 
-        amount += item.amount
-
+        
         const exp = document.querySelector('#expense')
         exp.innerHTML = `${amount}`
     }
-
-    
-  
 });
 
+const hamburger = document.querySelector('.hamburger')
+const profile = document.querySelector('.header')
+
+const cross = document.getElementById('cross')
+const transcations= document.querySelector('.Transcations')
+const info = document.querySelector('.info')
+
+cross.addEventListener('click' , function(){
+    profile.style.visibility = "hidden";
+    cross.style.visibility ="hidden" ;
+    hamburger.style.visibility="visible" ;
+    info.style.visibility = "visible" ;
+})
+
+
+hamburger.addEventListener('click' , function(){
+    profile.style.visibility = "visible" ;
+    hamburger.style.visibility="hidden" ;
+    cross.style.visibility = "visible" ;
+    //  transcations.style.visibility = "hidden" ;
+     info.style.visibility = "hidden" ;
+
+
+})
